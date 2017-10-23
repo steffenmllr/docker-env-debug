56c143384532419562ac9c55bc3b66f80536fd61
Steffen Müller <steffen@mllrsohn.com>
2017-10-23 17:24:01 +0200

    sort env my name

--- index.js
+++ index.js
@@ -2,9 +2,28 @@ const env = process.env;
 const http = require('http')
 const port = 3000
 
+
+function sortObject(o) {
+    var sorted = {},
+    key, a = [];
+
+    for (key in o) {
+        if (o.hasOwnProperty(key)) {
+            a.push(key);
+        }
+    }
+
+    a.sort();
+
+    for (key = 0; key < a.length; key++) {
+        sorted[a[key]] = o[a[key]];
+    }
+    return sorted;
+}
+
 const requestHandler = (request, response) => {
     response.writeHead(200, {'Content-Type': 'application/json'});
-    response.end(JSON.stringify(process.env, null, 4) + "\n");
+    response.end(JSON.stringify(sortObject(process.env), null, 4) + "\n");
 }
 
 const server = http.createServer(requestHandler);
